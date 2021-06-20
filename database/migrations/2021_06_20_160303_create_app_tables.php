2e2400be5b375c12387c1392c97dca34e267d9b8
Horino Yutaro <suzy.city16@gmail.com>
2021-06-20 16:17:28 +0900

    add migration

--- database/migrations/2021_06_20_160303_create_app_tables.php
+++ database/migrations/2021_06_20_160303_create_app_tables.php
@@ -13,8 +13,33 @@ class CreateAppTables extends Migration
      */
     public function up()
     {
-        Schema::create('app_tables', function (Blueprint $table) {
+        Schema::create('primary_categories', function (Blueprint $table) {
             $table->id();
+            $table->timestamps();
+        });
+
+        Schema::create('secondary_categories', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('primary_category_id');
+            $table->timestamps();
+            $table->foreign('primary_category_id')->references('id')->on('primary_categories');
+        });
+
+        Schema::create('item_conditions', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('seller_id');
+            $table->foreign('seller_id')->references('id')->on('users');
+            $table->unsignedBigInteger('buyer_id');
+            $table->foreign('buyer_id')->references('id')->on('users');
+            $table->unsignedBigInteger('secondary_category_id');
+            $table->foreign('secondary_category_id')->references('id')->on('secondary_categories');
+            $table->unsignedBigInteger('item_condition_id');
+            $table->foreign('item_condition_id')->references('id')->on('item_conditions');
             $table->timestamps();
         });
     }
@@ -26,6 +51,9 @@ class CreateAppTables extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('app_tables');
+        Schema::dropIfExists('items');
+        Schema::dropIfExists('item_conditions');
+        Schema::dropIfExists('secondary_categories');
+        Schema::dropIfExists('primary_categories');
     }
 }
